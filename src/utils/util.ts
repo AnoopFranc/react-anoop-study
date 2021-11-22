c78b045805f3dba37fce370baea310661a7e3979
Anoop <anoopfranciskk05@gmail.com.com>
2021-11-22 08:38:46 +0800

    added util hhmmss

---
+++ src/utils/util.ts
@@ -0,0 +1,12 @@
+export const msToHMS = ( ms: number ) => {
+    // 1- Convert to seconds:
+    let seconds =Math.ceil(ms / 1000) ;
+    // 2- Extract hours:
+    const hours = Math.floor(seconds / 3600) ; // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    const minutes = Math.floor(seconds / 60); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = seconds % 60;
+    return (hours+':'+minutes+':'+seconds)
+}
